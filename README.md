6af69770c8962897fa9505c1dfc95b7dd1db573c
Neo Zhen Ting <neozt99@gmail.com>
2025-07-18 21:49:46 +0800

    chore: reformat with prettier

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Tempermonkey user script to change the way season links are rendered in [Anichart](https://anichart.net/). The currently selected season is made to always appear in the middle, with the previous two and next two seasons on the left and right of it for seamless browsing between seasons.
 
-
 **How Anichart looks with this extension:**
 ![Screenshot of Anichart with extension enabled](docs/images/anichart-extension-enabled.png)
 Notice the season links at the top. Currently Summer 2024 is selected.
@@ -10,15 +9,17 @@ Notice the season links at the top. Currently Summer 2024 is selected.
 **Original:**
 ![Screenshot of Anichart without extension enabled](docs/images/anichart-original.png)
 
-
 ## Features
+
 1. Replace default season links to dynamically generated list centered around the currently selected season.
 2. Adds keyboard shortcuts to go to previous or next season (<kbd>Ctrl + Left Arrow</kbd> and <kbd>Ctrl + Right Arrow</kbd> by default).
 
 ## How to customise
+
 Customise the behaviour of this extension by modifying the `CONFIG` object at the top of the script.
 
 ## How to install
+
 1. Install [Tempermonkey](https://www.tampermonkey.net/) extension in your browser if you don't already have it.
 2. Add a new user script in Tempermonkey and replace the content of it with the content of `anichart-better-seasons.js` file and make sure it is enabled.
 3. (Optional) [Enable dev mode](https://www.tampermonkey.net/faq.php?locale=en#Q209) to allow user scripts to work on Chromium browsers.
--- anichart-better-seasons.js
+++ anichart-better-seasons.js
@@ -10,182 +10,198 @@
 // ==/UserScript==
 
 (function () {
-    'use strict'
+  "use strict";
 
-    /** Customize your config here **/
-    const CONFIG = {
-        replaceSeasonLinks: true, // set to false to use default Anichart season links
-        previousSeasonKey: 'ArrowLeft', // set undefined to disable previous season shortcut
-        previousSeasonKeyMod: 'ctrlKey', // undefined or 'ctrlKey' or 'altKey' or 'shiftKey'
-        nextSeasonKey: 'ArrowRight', // set undefined to disable next season shortcut
-        nextSeasonKeyMod: 'ctrlKey', // undefined or 'ctrlKey' or 'altKey' or 'shiftKey'
+  /** Customize your config here **/
+  const CONFIG = {
+    replaceSeasonLinks: true, // set to false to use default Anichart season links
+    previousSeasonKey: "ArrowLeft", // set undefined to disable previous season shortcut
+    previousSeasonKeyMod: "ctrlKey", // undefined or 'ctrlKey' or 'altKey' or 'shiftKey'
+    nextSeasonKey: "ArrowRight", // set undefined to disable next season shortcut
+    nextSeasonKeyMod: "ctrlKey", // undefined or 'ctrlKey' or 'altKey' or 'shiftKey'
+  };
+
+  const SEASONS = ["Winter", "Spring", "Summer", "Fall"];
+
+  console.log("[anichart-better-seasons] script running");
+
+  if (CONFIG.replaceSeasonLinks) {
+    window.navigation.addEventListener("navigate", () =>
+      setTimeout(replaceSeasonLinks),
+    );
+  }
+
+  if (CONFIG.previousSeasonKey || CONFIG.nextSeasonKey) {
+    document.addEventListener("keydown", handleKeyboardShortcuts);
+  }
+
+  /**
+   * Replace Anichart's season link at the top with season links centered around currently selected season.
+   */
+  function replaceSeasonLinks() {
+    const currentUrl = getUrl(window);
+    const currentSeason = extractSeason(currentUrl);
+
+    console.log(
+      `[anichart-better-seasons] currentSeason: ${JSON.stringify(currentSeason)}, currentUrl: ${currentUrl}`,
+    );
+
+    if (!currentSeason) {
+      return;
     }
 
-    const SEASONS = ['Winter', 'Spring', 'Summer', 'Fall']
+    const seasonsContainer = document.querySelector(".seasons");
+    const seasonLinks = [-2, -1, 0, 1, 2].map((i) =>
+      createSeasonLink(document, {
+        season: incrementSeason(currentSeason, i),
+        dataAttr: extractVueDataAttr(seasonsContainer),
+        activeLink: i === 0,
+      }),
+    );
+    seasonsContainer.replaceChildren(...seasonLinks);
+  }
 
-    console.log('[anichart-better-seasons] script running')
+  /**
+   * Navigate to previous or next season when keyboard shortcut is pressed.
+   * @param {KeyboardEvent} event
+   */
+  function handleKeyboardShortcuts(event) {
+    // console.log("[anichart-better-seasons] event", event) // Uncomment this line to find out what value to set for previousSeasonKey or nextSeasonKey
 
-    if (CONFIG.replaceSeasonLinks) {
-        window.navigation.addEventListener("navigate", () => setTimeout(replaceSeasonLinks))
+    const currentUrl = getUrl(window);
+    const currentSeason = extractSeason(currentUrl);
+
+    if (!currentSeason) {
+      return;
     }
 
-    if (CONFIG.previousSeasonKey || CONFIG.nextSeasonKey) {
-        document.addEventListener("keydown", handleKeyboardShortcuts)
+    if (
+      event.key === CONFIG.previousSeasonKey &&
+      (CONFIG.previousSeasonKeyMod === undefined ||
+        event[CONFIG.previousSeasonKeyMod])
+    ) {
+      const previousSeason = incrementSeason(currentSeason, -1);
+      window.location.assign(constructSeasonUrl(previousSeason));
+    } else if (
+      event.key === CONFIG.nextSeasonKey &&
+      (CONFIG.nextSeasonKeyMod === undefined || event[CONFIG.nextSeasonKeyMod])
+    ) {
+      const nextSeason = incrementSeason(currentSeason, 1);
+      window.location.assign(constructSeasonUrl(nextSeason));
+    }
+  }
+
+  /**
+   * Returns the current browser URL.
+   * @param {Window} window Browser window object
+   * @returns {string}
+   */
+  function getUrl(window) {
+    return window.location.href;
+  }
+
+  /**
+   * Extracts season's name and year from a URL.
+   * @param {string} url
+   * @returns {{name: string, year: number} | undefined}
+   */
+  function extractSeason(url) {
+    const splitted = url.split("/");
+    const lastPart = splitted[splitted.length - 1];
+    const [name, year] = lastPart.split("-");
+
+    const yearNumber = parseInt(year);
+
+    if (!SEASONS.includes(name) || isNaN(yearNumber)) {
+      return undefined;
     }
 
-    /**
-     * Replace Anichart's season link at the top with season links centered around currently selected season.
-     */
-    function replaceSeasonLinks() {
-        const currentUrl = getUrl(window)
-        const currentSeason = extractSeason(currentUrl)
+    return { name, year: yearNumber };
+  }
 
-        console.log(`[anichart-better-seasons] currentSeason: ${JSON.stringify(currentSeason)}, currentUrl: ${currentUrl}`)
+  /**
+   * Calculates the value of the next <code>offset</code> season.
+   * @param {{name: string, year: number}} current The current season
+   * @param {number} offset Can be positive or negative. Positive will calculate next season, negative will calculate previous season
+   * @returns {{name: string, year: number}}
+   */
+  function incrementSeason(current, offset) {
+    const NAME_OF_SEASONS = ["Winter", "Spring", "Summer", "Fall"];
+    const { name: currentSeasonName, year: currentYear } = current;
+    const currentSeasonIndex = NAME_OF_SEASONS.findIndex(
+      (name) => name === currentSeasonName,
+    );
 
-        if (!currentSeason) {
-            return
-        }
+    const deltaYear = Math.floor(
+      (currentSeasonIndex + offset) / NAME_OF_SEASONS.length,
+    );
 
-        const seasonsContainer = document.querySelector('.seasons')
-        const seasonLinks = [-2, -1, 0, 1, 2]
-            .map(i =>
-                createSeasonLink(document, {
-                    season: incrementSeason(currentSeason, i),
-                    dataAttr: extractVueDataAttr(seasonsContainer),
-                    activeLink: i === 0,
-                })
-            )
-        seasonsContainer.replaceChildren(...seasonLinks)
+    const newName =
+      NAME_OF_SEASONS[mod(currentSeasonIndex + offset, NAME_OF_SEASONS.length)];
+    const newYear = currentYear + deltaYear;
+
+    return { name: newName, year: newYear };
+  }
+
+  /**
+   *
+   * @param {{name: string, year: number}} season
+   * @returns {string} New relative url that points to <code>season</code>
+   */
+  function constructSeasonUrl(season) {
+    return `/${season.name}-${season.year}`;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} node
+   * @returns {undefined|string}
+   */
+  function extractVueDataAttr(node) {
+    const rawDataAttr = Object.keys(node.dataset)?.[0];
+    if (!rawDataAttr) {
+      return undefined;
     }
 
-    /**
-     * Navigate to previous or next season when keyboard shortcut is pressed.
-     * @param {KeyboardEvent} event
-     */
-    function handleKeyboardShortcuts(event) {
-        // console.log("[anichart-better-seasons] event", event) // Uncomment this line to find out what value to set for previousSeasonKey or nextSeasonKey
+    return `${rawDataAttr[0]}-${rawDataAttr.substring(1)}`;
+  }
 
-        const currentUrl = getUrl(window)
-        const currentSeason = extractSeason(currentUrl)
+  /**
+   *
+   * @param {Document} document
+   * @param {{name: string, year: number}} season
+   * @param {string} dataAttr
+   * @param {boolean} activeLink Whether the link is currently selected
+   * @returns {HTMLAnchorElement} Anchor tag used to redirect the browser to `season` page
+   */
+  function createSeasonLink(document, { season, dataAttr, activeLink }) {
+    const seasonNameDiv = document.createElement("div");
+    seasonNameDiv.className = "season-name";
+    seasonNameDiv.innerText = season.name;
+    seasonNameDiv.setAttribute(`data-${dataAttr}`, undefined);
 
-        if (!currentSeason) {
-            return
-        }
+    const seasonYearDiv = document.createElement("div");
+    seasonYearDiv.className = "season-year";
+    seasonYearDiv.innerText = season.year.toString();
+    seasonYearDiv.setAttribute(`data-${dataAttr}`, undefined);
 
-        if (event.key === CONFIG.previousSeasonKey && (CONFIG.previousSeasonKeyMod === undefined || event[CONFIG.previousSeasonKeyMod])) {
-            const previousSeason = incrementSeason(currentSeason, -1)
-            window.location.assign(constructSeasonUrl(previousSeason))
-        } else if (event.key === CONFIG.nextSeasonKey && (CONFIG.nextSeasonKeyMod === undefined || event[CONFIG.nextSeasonKeyMod])) {
-            const nextSeason = incrementSeason(currentSeason, 1)
-            window.location.assign(constructSeasonUrl(nextSeason))
-        }
-    }
+    const anchor = document.createElement("a");
+    anchor.href = constructSeasonUrl(season);
+    anchor.className = activeLink
+      ? "season router-link-exact-active router-link-active"
+      : "season";
+    anchor.setAttribute(`data-${dataAttr}`, undefined);
+    anchor.append(seasonNameDiv, seasonYearDiv);
+    return anchor;
+  }
 
-    /**
-     * Returns the current browser URL.
-     * @param {Window} window Browser window object
-     * @returns {string}
-     */
-    function getUrl(window) {
-        return window.location.href
-    }
-
-    /**
-     * Extracts season's name and year from a URL.
-     * @param {string} url
-     * @returns {{name: string, year: number} | undefined}
-     */
-    function extractSeason(url) {
-        const splitted = url.split('/')
-        const lastPart = splitted[splitted.length - 1]
-        const [name, year] = lastPart.split('-')
-
-        const yearNumber = parseInt(year)
-
-        if (!SEASONS.includes(name) || isNaN(yearNumber)) {
-            return undefined
-        }
-
-        return {name, year: yearNumber}
-    }
-
-    /**
-     * Calculates the value of the next <code>offset</code> season.
-     * @param {{name: string, year: number}} current The current season
-     * @param {number} offset Can be positive or negative. Positive will calculate next season, negative will calculate previous season
-     * @returns {{name: string, year: number}}
-     */
-    function incrementSeason(current, offset) {
-        const NAME_OF_SEASONS = ['Winter', 'Spring', 'Summer', 'Fall']
-        const {name: currentSeasonName, year: currentYear} = current
-        const currentSeasonIndex = NAME_OF_SEASONS.findIndex(name => name === currentSeasonName)
-
-        const deltaYear = Math.floor((currentSeasonIndex + offset) / NAME_OF_SEASONS.length)
-
-        const newName = NAME_OF_SEASONS[mod(currentSeasonIndex + offset, NAME_OF_SEASONS.length)]
-        const newYear = currentYear + deltaYear
-
-        return {name: newName, year: newYear}
-    }
-
-    /**
-     *
-     * @param {{name: string, year: number}} season
-     * @returns {string} New relative url that points to <code>season</code>
-     */
-    function constructSeasonUrl(season) {
-        return `/${season.name}-${season.year}`
-    }
-
-
-    /**
-     *
-     * @param {HTMLElement} node
-     * @returns {undefined|string}
-     */
-    function extractVueDataAttr(node) {
-        const rawDataAttr = Object.keys(node.dataset)?.[0]
-        if (!rawDataAttr) {
-            return undefined
-        }
-
-        return `${rawDataAttr[0]}-${rawDataAttr.substring(1)}`
-    }
-
-    /**
-     *
-     * @param {Document} document
-     * @param {{name: string, year: number}} season
-     * @param {string} dataAttr
-     * @param {boolean} activeLink Whether the link is currently selected
-     * @returns {HTMLAnchorElement} Anchor tag used to redirect the browser to `season` page
-     */
-    function createSeasonLink(document, {season, dataAttr, activeLink}) {
-        const seasonNameDiv = document.createElement('div')
-        seasonNameDiv.className = 'season-name'
-        seasonNameDiv.innerText = season.name
-        seasonNameDiv.setAttribute(`data-${dataAttr}`, undefined)
-
-        const seasonYearDiv = document.createElement('div')
-        seasonYearDiv.className = 'season-year'
-        seasonYearDiv.innerText = season.year.toString()
-        seasonYearDiv.setAttribute(`data-${dataAttr}`, undefined)
-
-        const anchor = document.createElement('a')
-        anchor.href = constructSeasonUrl(season)
-        anchor.className = activeLink ? 'season router-link-exact-active router-link-active' : 'season'
-        anchor.setAttribute(`data-${dataAttr}`, undefined)
-        anchor.append(seasonNameDiv, seasonYearDiv)
-        return anchor
-    }
-
-    /**
-     * Calculate a mod b. Caters for negative values as well, unlike Javascript's native % operator.
-     * @param a
-     * @param b
-     * @returns {number}
-     */
-    function mod(a, b) {
-        return ((a % b) + b) % b
-    }
-})()
+  /**
+   * Calculate a mod b. Caters for negative values as well, unlike Javascript's native % operator.
+   * @param a
+   * @param b
+   * @returns {number}
+   */
+  function mod(a, b) {
+    return ((a % b) + b) % b;
+  }
+})();
